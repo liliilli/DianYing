93cbc9d24d0091a3501179e248eb83c149d2ac0b
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2018-08-28 23:06:37 +0900

    Add subsidary third-party libraries

--- DianYing/Libraries/imgui/imgui/imgui_impl_opengl3.cpp
+++ DianYing/Libraries/imgui/imgui/imgui_impl_opengl3.cpp
@@ -28,7 +28,7 @@
 
 #include "imgui.h"
 #include "imgui_impl_opengl3.h"
-#include <GL/glew.h>    // This example is using gl3w to access OpenGL functions. You may freely use any other OpenGL loader such as: glew, glad, glLoadGen, etc.
+#include <glad/glad.h>    // This example is using gl3w to access OpenGL functions. You may freely use any other OpenGL loader such as: glew, glad, glLoadGen, etc.
 
 // OpenGL Data
 static char         g_GlslVersion[32] = "#version 150";
