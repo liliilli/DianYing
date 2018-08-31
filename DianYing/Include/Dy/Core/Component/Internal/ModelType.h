ac500b35d8456487b91cc3d0c54655c7d21dcf60
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2018-09-01 01:49:49 +0900

    Auto stash before merge of "feat_basic_renderer" and "feat_sep_info_resrc"

---
+++ DianYing/Include/Dy/Core/Component/Internal/ModelType.h
@@ -0,0 +1,33 @@
+#ifndef GUARD_DY_CORE_COMPONENT_INTERNAL_MODEL_TYPE_H
+#define GUARD_DY_CORE_COMPONENT_INTERNAL_MODEL_TYPE_H
+///
+/// MIT License
+/// Copyright (c) 2018 Jongmin Yun
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+///
+
+#include <string>
+
+namespace dy
+{
+
+///
+/// @struct PDyModelConstructionDescriptor
+/// @brief
+///
+struct PDyModelConstructionDescriptor final
+{
+  std::string                           mModelName        = "";
+  std::string                           mModelPath        = "";
+};
+
+} /// ::dy namespace
+
+#endif /// GUARD_DY_CORE_COMPONENT_INTERNAL_MODEL_TYPE_H
