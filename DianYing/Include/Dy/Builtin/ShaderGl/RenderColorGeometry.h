ff45bb883758c920f40c6ba55fdf52a79c98199b
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2018-09-23 13:07:08 +0900

    [Add][Builtin] Color with arbitary geometry

---
+++ DianYing/Include/Dy/Builtin/ShaderGl/RenderColorGeometry.h
@@ -0,0 +1,32 @@
+#ifndef GUARD_DY_BUILTIN_SHADERGL_RENDERCOLORGEOMETRY_H
+#define GUARD_DY_BUILTIN_SHADERGL_RENDERCOLORGEOMETRY_H
+///
+/// MIT License
+/// Copyright (c) 2018 Jongmin Yun
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+///
+
+namespace dy::builtin
+{
+
+///
+/// @class FDyBuiltinShaderGLRenderColorGeometry
+/// @brief Just instantiate render color geometry shader information and resources.
+///
+class FDyBuiltinShaderGLRenderColorGeometry final
+{
+public:
+  FDyBuiltinShaderGLRenderColorGeometry();
+  inline static constexpr std::string_view sName = "dyBtShaderGlRenderColGeom";
+};
+
+} /// ::dy::builtin namespace
+
+#endif /// GUARD_DY_BUILTIN_SHADERGL_RENDERCOLORGEOMETRY_H
