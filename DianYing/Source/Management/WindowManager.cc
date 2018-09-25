d842ad0e5231cf6ba641a1939a047086548039c5
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2018-09-25 15:56:09 +0900

    [Refc] Move WIN32API not used code to file.

--- DianYing/Source/Management/WindowManager.cc
+++ DianYing/Source/Management/WindowManager.cc
@@ -15,7 +15,6 @@
 /// Header file
 #include <Dy/Management/WindowManager.h>
 
-#include <iostream>
 #include <future>
 
 #include <imgui/imgui.h>
@@ -40,9 +39,8 @@
 #include <Dy/Builtin/ShaderGl/RenderPass.h>
 #include <Dy/Builtin/ShaderGl/RenderColorGeometry.h>
 
-#include <fmod.hpp>
-#include "Dy/Core/Component/Resource/SoundResource.h"
-#include "Dy/Management/HeapResourceManager.h"
+#include <Dy/Management/HeapResourceManager.h>
+#include <Dy/Management/SoundManager.h>
 
 ///
 /// Undefined proprocessor WIN32 macro "max, min" for preventing misuse.
@@ -371,91 +369,6 @@ namespace
 {
 
 ///
-/// @brief Callback function of WIN32 instance handle.
-///
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-  switch (uMsg) {
-  case WM_ACTIVATE:
-    if (LOWORD(wParam) == WA_INACTIVE)
-         { std::cout << "Inactivated\n"; }
-    else { std::cout << "Activated\n";   }
-    break;
-  case WM_CLOSE:
-#ifdef false
-    DestroyWindow(windowHandle);
-#endif
-    PostQuitMessage(0);
-    break;
-  case WM_PAINT:
-#ifdef false
-    ValidateRect(windowHandle, nullptr);
-#endif
-    break;
-  default: return DefWindowProc(hWnd, uMsg, wParam, lParam);
-  }
-
-  return S_OK;
-}
-
-///
-/// @brief Setup windows instance.
-///
-[[nodiscard]] EDySuccess DyWin32InitializeWindow(HINSTANCE hInstance, HWND* windowHandle) {
-  WNDCLASSEX wndClass;
-  wndClass.cbSize       = sizeof(WNDCLASSEX);
-  wndClass.style        = CS_HREDRAW | CS_VREDRAW;
-  wndClass.lpfnWndProc  = WndProc;
-  wndClass.cbClsExtra   = 0;
-  wndClass.cbWndExtra   = 0;
-  wndClass.hInstance    = hInstance;
-  wndClass.hIcon        = LoadIcon(nullptr, IDI_APPLICATION);
-  wndClass.hCursor      = LoadIcon(nullptr, IDC_ARROW);
-  wndClass.hbrBackground= static_cast<HBRUSH>(GetStockObject(BLACK_BRUSH));
-  wndClass.lpszMenuName = nullptr;
-  wndClass.lpszClassName= L"DianYing";
-  wndClass.hIconSm      = LoadIcon(nullptr, IDI_WINLOGO);
-
-  if (!RegisterClassEx(&wndClass)) {
-    std::cout << "Faeild to register window class.\n";
-    fflush(stdout);
-    exit(1);
-  }
-
-  const auto& settingManager = dy::MDySetting::GetInstance();
-
-  const DWORD dwordExStyle = WS_EX_APPWINDOW | WS_EX_WINDOWEDGE;
-  const DWORD dwordStyle   = WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
-  RECT  windowRect;
-  windowRect.left    = 0L;
-  windowRect.top     = 0L;
-  windowRect.right   = settingManager.GetWindowSizeWidth();
-  windowRect.bottom  = settingManager.GetWindowSizeHeight();
-  AdjustWindowRectEx(&windowRect, dwordStyle, FALSE, dwordExStyle);
-
-  *windowHandle = CreateWindowEx(0,
-      L"DianYing",
-      L"DianYing v0.0.1",
-      dwordStyle | WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
-      64L,
-      64L,
-      windowRect.right - windowRect.left,
-      windowRect.bottom - windowRect.top,
-      nullptr,
-      nullptr,
-      hInstance,
-      nullptr);
-
-  if (!*windowHandle) {
-    const DWORD errorCode = GetLastError();
-    printf("Failed to create window handle with %lx\n", errorCode);
-    fflush(stdout);
-    exit(1);
-  }
-
-  return DY_SUCCESS;
-}
-
-///
 /// @brief Callback method for size check and resizing.
 ///
 void DyGlCallbackFrameBufferSize(GLFWwindow* window, int width, int height)
@@ -475,9 +388,7 @@ void DyGlCallbackWindowClose(GLFWwindow* window)
 
 } /// unnamed namespace
 #elif defined(MDY_PLATFORM_FLAG_LINUX)
-namespace { } /// unnamed namespace
 #elif defined(MDY_PLATFORM_FLAG_MACOS)
-namespace { } /// unnamed namespace
 #endif
 
 //!
@@ -490,10 +401,13 @@ namespace dy
 void MDyWindow::Run()
 {
   auto& timeManager  = MDyTime::GetInstance();
+  MDY_CALL_ASSERT_SUCCESS(MDySound::GetInstance().PlaySoundElement("1"));
 
   while (!glfwWindowShouldClose(this->mGlfwWindow))
   {
     timeManager.pUpdate();
+    if (auto& instance = MDySound::GetInstance(); true) { instance.Update(MDY_NOT_INITIALIZED_M1); }
+
     if (timeManager.IsGameFrameTicked() == DY_SUCCESS)
     {
       const auto dt = timeManager.GetGameScaledTickedDeltaTimeValue();
@@ -502,30 +416,6 @@ void MDyWindow::Run()
       this->pRender();
     }
   }
-#ifdef false
-#if defined(_WIN32)
-  MSG   win32Message;
-  bool  win32QuitMessageReceived = false;
-  while (!win32QuitMessageReceived) {
-    while (PeekMessage(&win32Message, nullptr, 0, 0, PM_REMOVE)) {
-      TranslateMessage(&win32Message);
-      DispatchMessage(&win32Message);
-
-      if (win32Message.message == WM_QUIT) {
-        win32QuitMessageReceived = true;
-        bDyWindowCloseSign = true;
-        break;
-      }
-    }
-
-    if (!IsIconic(windowHandle)) {
-      SwapBuffers(windowDeviceContext);
-    }
-  }
-#elif (__linux__)
-  assert(false);
-#endif
-#endif
 }
 
 ///
