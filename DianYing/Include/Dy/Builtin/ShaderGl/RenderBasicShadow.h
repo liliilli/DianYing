b9c7f16e79991f078e6427a7aca23c24c4d1b13e
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2018-09-25 22:18:58 +0900

    [Add] Builtin Basic Shader instance

---
+++ DianYing/Include/Dy/Builtin/ShaderGl/RenderBasicShadow.h
@@ -0,0 +1,32 @@
+#ifndef GUARD_DY_BUILTIN_SHADERGL_RENDERBASICSHADOW_H
+#define GUARD_DY_BUILTIN_SHADERGL_RENDERBASICSHADOW_H
+///
+/// MIT License
+/// Copyright (c) 2018 Jongmin Yun
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+///
+
+namespace dy::builtin
+{
+
+///
+/// @class FDyBuiltinShaderGLRenderBasicShadow
+/// @brief Instantiate and create resource for basic shadow rendering.
+///
+class FDyBuiltinShaderGLRenderBasicShadow final
+{
+public:
+  FDyBuiltinShaderGLRenderBasicShadow();
+  inline static constexpr std::string_view sName = "dyBtShaderGlRenderBscShadow";
+};
+
+} /// ::dy::builtin namespace
+
+#endif /// GUARD_DY_BUILTIN_SHADERGL_RENDERBASICSHADOW_H
