2ae65b383b15a9548c03ed3c0f194526b6d51ccf
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2018-09-26 15:56:42 +0900

    [Add][System] PhysX customized callback function,

---
+++ DianYing/Include/Dy/Management/Helper/PhysXErrorCallback.h
@@ -0,0 +1,36 @@
+#ifndef GUARD_DY_MANAGEMENT_HELPER_PHYSXERRORCALLBACK_H
+#define GUARD_DY_MANAGEMENT_HELPER_PHYSXERRORCALLBACK_H
+///
+/// MIT License
+/// Copyright (c) 2018 Jongmin Yun
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+///
+
+#include <foundation/PxErrorCallback.h>
+
+namespace dy
+{
+
+///
+/// @class FDyPhysXErrorCallback
+/// @brief PhysX internal error delegator type.
+///
+class FDyPhysXErrorCallback final : public physx::PxErrorCallback
+{
+public:
+  ///
+  /// @brief error callback function.
+  ///
+  void reportError(physx::PxErrorCode::Enum code, const char* message, const char* file, int line) override final;
+};
+
+} /// ::dy namespace
+
+#endif /// GUARD_DY_MANAGEMENT_HELPER_PHYSXERRORCALLBACK_H
--- DianYing/Source/Entry/entry.cc
+++ DianYing/Source/Entry/entry.cc
@@ -29,6 +29,9 @@
 #include <Dy/Management/Editor/GuiManager.h>
 #include <Dy/Helper/Pointer.h>
 
+#include <PxPhysicsAPI.h>
+#include "Dy/Management/Helper/PhysXErrorCallback.h"
+
 namespace
 {
 
@@ -166,6 +169,16 @@ EDySuccess __DyReleaseWin32Debug()
 #define MDY_WIN32_TRY_TURN_OFF_DEBUG()  (void)0
 #endif
 
+physx::PxDefaultAllocator     gAllocator;
+dy::FDyPhysXErrorCallback     gCallback;
+physx::PxPvd*                 gPvd;
+physx::PxFoundation*          gFoundation;
+physx::PxPhysics*             gPhysicx;
+physx::PxCpuDispatcher*       gDispatcher;
+physx::PxScene*               gScene;
+physx::PxMaterial*            gMaterial;
+physx::PxReal                 stackZ = 10.0f;
+
 ///
 /// @brief Main function of win32 / win64 platform.
 ///
@@ -188,6 +201,97 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLin
   DyInitiailzeAllManagers();
 
   DyTempInitializeTestResources();
+
+  {
+    gFoundation = PxCreateFoundation(PX_FOUNDATION_VERSION, gAllocator, gCallback);
+    gPhysicx = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, physx::PxTolerancesScale(), true, gPvd);
+
+    physx::PxSceneDesc sceneDescriptor{gPhysicx->getTolerancesScale()};
+    sceneDescriptor.gravity = physx::PxVec3{0, -9.81f, 0};
+
+    gDispatcher = physx::PxDefaultCpuDispatcherCreate(2);
+    sceneDescriptor.cpuDispatcher = gDispatcher;
+    sceneDescriptor.filterShader  = physx::PxDefaultSimulationFilterShader;
+
+    gScene = gPhysicx->createScene(sceneDescriptor);
+
+    physx::PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
+    if (pvdClient)
+    {
+      pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+      pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+      pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+    }
+
+    gMaterial = gPhysicx->createMaterial(0.5f, 0.5f, 0.6f);
+    physx::PxRigidStatic *groundPlane = PxCreatePlane(*gPhysicx, physx::PxPlane(0, 1, 0, 0), *gMaterial);
+
+    gScene->addActor(*groundPlane);
+
+    ///
+    /// @function
+    /// @brief
+    ///
+    static auto CreateStack = [](const physx::PxTransform &t, physx::PxU32 size, physx::PxReal halfExtent)
+    {
+      using namespace physx;
+
+      PxShape *shape = gPhysicx->createShape(PxBoxGeometry(halfExtent, halfExtent, halfExtent), *gMaterial);
+      for (PxU32 i = 0; i < size; i++)
+      {
+        for (PxU32 j = 0; j < size - i; j++)
+        {
+          const PxTransform localTm(PxVec3(PxReal(j * 2) - PxReal(size - i), PxReal(i * 2 + 1), 0) * halfExtent);
+
+          PxRigidDynamic *body = gPhysicx->createRigidDynamic(t.transform(localTm));
+          body->attachShape(*shape);
+
+          PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
+          gScene->addActor(*body);
+        }
+      }
+      shape->release();
+    };
+
+    ///
+    /// @function
+    /// @brief
+    ///
+    static auto CreateDynamic = [](const physx::PxTransform &t, const physx::PxGeometry &geometry, const physx::PxVec3 &velocity = physx::PxVec3(0)) -> physx::PxRigidDynamic*
+    {
+      using namespace physx;
+
+      PxRigidDynamic *dynamic = PxCreateDynamic(*gPhysicx, t, geometry, *gMaterial, 10.0f);
+      dynamic->setAngularDamping(0.5f);
+      dynamic->setLinearVelocity(velocity);
+      gScene->addActor(*dynamic);
+
+      return dynamic;
+    };
+
+    for (physx::PxU32 i = 0; i < 5; i++)
+    {
+      CreateStack(physx::PxTransform(physx::PxVec3(0, 0, stackZ -= 10.0f)), 10, 2.0f);
+    }
+
+    CreateDynamic(physx::PxTransform(physx::PxVec3(0, 40, 100)), physx::PxSphereGeometry(10), physx::PxVec3(0, -50, -100));
+  }
+
+  // PhysX step physics
+  for (TI32 i = 0; i < 100; ++i)
+  {
+    gScene->simulate(1.0f / 60.0f);
+    gScene->fetchResults(true);
+  }
+
+  // PhysX test release
+  {
+    gScene->release();
+    gPhysicx->release();
+    gFoundation->release();
+
+    printf("SnippetHelloWorld done.\n");
+  }
 
   MDY_LOG_INFO_D("Running application routine.");
   dy::MDyWindow::GetInstance().Run();
