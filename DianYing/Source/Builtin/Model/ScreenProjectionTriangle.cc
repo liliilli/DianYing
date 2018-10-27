2c2044862ca63d308926d0437d526755bfcb8b10
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2018-10-28 00:56:46 +0900

    [Add][Builtin] FDyBuiltinModelScreenProjectionTriangle for screen render

---
+++ DianYing/Source/Builtin/Model/ScreenProjectionTriangle.cc
@@ -0,0 +1,54 @@
+#include <precompiled.h>
+///
+/// MIT License
+/// Copyright (c) 2018 Jongmin Yun
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+///
+
+/// Header file
+#include <Dy/Builtin/Model/ScreenProjectionTriangle.h>
+#include <Dy/Core/Component/Internal/ModelType.h>
+#include <Dy/Management/DataInformationManager.h>
+
+namespace dy::builtin
+{
+
+FDyBuiltinModelScreenProjectionTriangle::FDyBuiltinModelScreenProjectionTriangle()
+{
+  PDySubmeshInformationDescriptor submeshDesc;
+
+  // Make position, normal, texcoord vertex information
+  submeshDesc.mVertices.reserve(4);
+  {
+    DDyVertexInformation vertex;
+
+    vertex.mPosition = DDyVector3{ -1, -1, 0 };
+    vertex.mTexCoords = DDyVector2{ 0, 0 };
+    submeshDesc.mVertices.emplace_back(vertex);
+    vertex.mPosition = DDyVector3{ 3, -1, 0 };
+    vertex.mTexCoords = DDyVector2{ 2, 0 };
+    submeshDesc.mVertices.emplace_back(vertex);
+    vertex.mPosition = DDyVector3{ -1, 3, 0 };
+    vertex.mTexCoords = DDyVector2{ 0, 2 };
+    submeshDesc.mVertices.emplace_back(vertex);
+  }
+
+  // Make model information
+  PDyModelConstructionVertexDescriptor modelDesc;
+  {
+    modelDesc.mModelName = FDyBuiltinModelScreenProjectionTriangle::sName;
+    modelDesc.mSubmeshConstructionInformations.emplace_back(submeshDesc);
+  }
+
+  auto& manInfo = MDyDataInformation::GetInstance();
+  MDY_CALL_ASSERT_SUCCESS(manInfo.CreateModelInformation(modelDesc));
+}
+
+} /// ::dy::builtin namespace
