6864af23b8b842b8b88ea677fcbb2c919a5bb5c8
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2018-11-10 21:47:27 +0900

    [Refactor] Implement Static core initilaization/release type.

---
+++ DianYing/Source/Core/Etc/StaticCoreTrigger.cc
@@ -0,0 +1,128 @@
+#include <precompiled.h>
+///
+/// MIT License
+/// Copyright (c) 2018 Jongmin Yun
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+///
+
+/// Header file
+#include <Dy/Core/Etc/StaticCoreTrigger.h>
+
+#include <Dy/Management/DataInformationManager.h>
+#include <Dy/Management/HeapResourceManager.h>
+#include <Dy/Management/InputManager.h>
+#include <Dy/Management/LoggingManager.h>
+#include <Dy/Management/MetaInfoManager.h>
+#include <Dy/Management/PhysicsManager.h>
+#include <Dy/Management/RenderingManager.h>
+#include <Dy/Management/SettingManager.h>
+#include <Dy/Management/SoundManager.h>
+#include <Dy/Management/SynchronizationManager.h>
+#include <Dy/Management/TimeManager.h>
+#include <Dy/Management/WindowManager.h>
+#include <Dy/Management/WorldManager.h>
+#include <Dy/Management/FontManager.h>
+#include <Dy/Management/ScriptManager.h>
+#include <Dy/Management/Editor/GuiManager.h>
+
+namespace dy
+{
+
+void SDyCoreTrigger::InitiailzeAllManagers()
+{
+  ///
+  /// @brief Forward runtime arguments to setting manager.
+  ///
+  static auto InsertExecuteRuntimeArguments = []()
+  {
+    auto& settingManager = dy::MDySetting::GetInstance();
+
+    #if defined(MDY_PLATFORM_FLAG_WINDOWS) && defined(_WIN32)
+      const TI32 size = __argc;
+      for (TI32 i = 0; i < size; ++i) { settingManager.pfArgsPushback(__argv[i]); }
+    #elif defined(MDY_PLATFORM_FLAG_LINUX) && defined(__linux__)
+      static_assert(false, "Linux does not support now.");
+    #elif defined(MDY_PLATFORM_FLAG_MACOS)
+      static_assert(false, "Macos does not support now.");
+    #endif
+  };
+
+  ///
+  /// @brief Set logging feature.
+  ///
+  static auto FirstSetLoggingFeature = []()
+  {
+    auto& logManager = dy::MDySetting::GetInstance();
+    logManager.SetSubFeatureLoggingToConsole(true);
+    logManager.SetSubFeatureLoggingToFile(true);
+  };
+
+  //! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  //! FUNCTIONBODY ∨
+  //! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  InsertExecuteRuntimeArguments();
+  MDY_CALL_ASSERT_SUCCESS(dy::MDySetting::Initialize());
+
+  FirstSetLoggingFeature();
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyLog::Initialize());
+#if defined(MDY_FLAG_IN_EDITOR)
+  MDY_CALL_ASSERT_SUCCESS(dy::editor::MDyEditorGui::Initialize());
+#endif
+
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyTime::Initialize());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyDataInformation::Initialize());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyHeapResource::Initialize());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyWorld::Initialize());
+
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyMetaInfo::Initialize());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyWindow::Initialize());
+
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyRendering::Initialize());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyInput::Initialize());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDySound::Initialize());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyPhysics::Initialize());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyFont::Initialize());
+
+  MDY_CALL_ASSERT_SUCCESS(dy::MDySync::Initialize());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyScript::Initialize());
+  MDY_LOG_WARNING_D("========== DIANYING MANAGER INITIALIZED ==========");
+}
+
+void SDyCoreTrigger::ReleaseAllManagers()
+{
+  MDY_LOG_WARNING_D("========== DIANYING MANAGER RELEASED ==========");
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyScript::Release());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDySync::Release());
+
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyFont::Release());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyPhysics::Release());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDySound::Release());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyInput::Release());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyRendering::Release());
+
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyWindow::Release());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyMetaInfo::Release());
+
+  // Release other management instance.
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyWorld::Release());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyHeapResource::Release());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyDataInformation::Release());
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyTime::Release());
+#if defined(MDY_FLAG_IN_EDITOR)
+  MDY_CALL_ASSERT_SUCCESS(dy::editor::MDyEditorGui::Release());
+#endif
+
+  MDY_CALL_ASSERT_SUCCESS(dy::MDyLog::Release());
+
+  MDY_CALL_ASSERT_SUCCESS(dy::MDySetting::Release());
+}
+
+} /// ::dy namespace
