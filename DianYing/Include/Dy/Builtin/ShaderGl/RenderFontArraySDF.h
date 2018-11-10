afd4cfd7389e2dffb63af5cff209a99b07ffba63
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2018-11-10 17:30:04 +0900

    [Add][Builtin] GL_TEXTURE_2D_ARRAY font rendering program.

---
+++ DianYing/Include/Dy/Builtin/ShaderGl/RenderFontArraySDF.h
@@ -0,0 +1,34 @@
+#ifndef GUARD_DY_BUILTIN_SHADERGL_RENDERFONTARRAYSDF_H
+#define GUARD_DY_BUILTIN_SHADERGL_RENDERFONTARRAYSDF_H
+///
+/// MIT License
+/// Copyright (c) 2018 Jongmin Yun
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+///
+
+namespace dy::builtin
+{
+
+///
+/// @class FDyBuiltinShaderGLRenderFontArraySDF
+/// @brief Instantiate and create resource for basic shadow rendering.
+///
+class FDyBuiltinShaderGLRenderFontArraySDF final
+{
+public:
+  FDyBuiltinShaderGLRenderFontArraySDF();
+
+  /// Constant name for creating DDyShaderInformation
+  inline static MDY_SET_IMMUTABLE_STRING(sName, "dyBtShaderGlRenderFontArraySDF");
+};
+
+} /// ::dy::builtin namespace
+
+#endif /// GUARD_DY_BUILTIN_SHADERGL_RENDERFONTARRAYSDF_H
