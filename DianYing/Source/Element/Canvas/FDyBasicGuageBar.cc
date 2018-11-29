1749b3502db38a69ab12b876765523409b3b4301
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2018-11-29 15:21:52 +0900

    [Add][Feat] Basic UI guage bar object.

---
+++ DianYing/Source/Element/Canvas/FDyBasicGuageBar.cc
@@ -0,0 +1,135 @@
+#include <precompiled.h>
+///
+/// MIT License
+/// Copyright (c) 2018 Jongmin Yun
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+///
+
+/// Header file
+#include <Dy/Element/Canvas/FDyBasicGaugeBar.h>
+
+namespace dy
+{
+
+EDySuccess FDyBasicGaugeBar::Initialize(const PDyMetaWidgetBarDescriptor& objectMetaDesc)
+{
+  //! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  //! FUNCTIONBODY ∨
+  //! - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  // Set properties.
+  this->pSetObjectName(objectMetaDesc.mUiObjectSpecifierName);
+
+  this->mForegroundColor        = objectMetaDesc.mForegroundColor;
+  this->mBackgroundColor        = objectMetaDesc.mBackgroundColor;
+  this->mIsUsingBackgroundColor = objectMetaDesc.mIsUsingBackground;
+  this->mPadding                = objectMetaDesc.mPadding;
+  this->mValues.mMin            = objectMetaDesc.mMin;
+  this->mValues.mMax            = objectMetaDesc.mMax;
+
+  this->SetWidgetCentralPosition(objectMetaDesc.mInitialPosition);
+  this->SetWidgetFrameSize      (objectMetaDesc.mWidgetSize);
+  this->mOrigin                 = objectMetaDesc.mOrigin;
+
+  // Initialize FontRenderer.
+  PDyBasicGaugeBarRendererCtorInformation desc = {};
+  desc.mPtrUiObject = this;
+  MDY_CALL_ASSERT_SUCCESS(this->mRenderer.Initialize(desc));
+
+  return DY_SUCCESS;
+}
+
+void FDyBasicGaugeBar::Release()
+{
+  this->mRenderer.Release();
+}
+
+TF32 FDyBasicGaugeBar::GetPresentValue() const noexcept
+{
+  return this->mValues.mPresent;
+}
+
+TF32 FDyBasicGaugeBar::GetMinimumValue() const noexcept
+{
+  return this->mValues.mMin;
+}
+
+TF32 FDyBasicGaugeBar::GetMaximumValue() const noexcept
+{
+  return this->mValues.mMax;
+}
+
+TF32 FDyBasicGaugeBar::GetPercentage() const noexcept
+{
+  return this->mValues.mPercentage;
+}
+
+const DDyColorRGBA& FDyBasicGaugeBar::GetForegroundColor() const noexcept
+{
+  return this->mForegroundColor;
+}
+
+const DDyColorRGBA& FDyBasicGaugeBar::GetBackgroundColor() const noexcept
+{
+  return this->mBackgroundColor;
+}
+
+void FDyBasicGaugeBar::SetPresentValue(_MIN_ const TF32 value) noexcept
+{
+  this->mValues.mPresent = value;
+  this->pUpdatePercentage();
+}
+
+void FDyBasicGaugeBar::SetForegroundColor(_MIN_ const DDyColorRGBA& color)
+{
+  this->mForegroundColor = color;
+}
+
+void FDyBasicGaugeBar::SetBackgroundColor(_MIN_ const DDyColorRGBA& color)
+{
+  this->mBackgroundColor = color;
+}
+
+void FDyBasicGaugeBar::Render()
+{
+  this->mRenderer.Render();
+}
+
+const DDyVector2& FDyBasicGaugeBar::GetRenderPosition() const noexcept
+{
+  return this->mFinalCentralPosition;
+}
+
+const TI32 FDyBasicGaugeBar::GetPadding() const noexcept
+{
+  return this->mPadding;
+}
+
+void FDyBasicGaugeBar::AlignFinalPosition(_MIN_ const DDyVector2& parentFinalPosition, _MIN_ const DDyVectorInt2& parentFrameSize)
+{
+  this->mFinalCentralPosition =
+    DyGetPositionWithOrigin(parentFinalPosition, parentFrameSize, this->mOrigin)
+    + this->GetWidgetPosition(EDyOrigin::Center_Center);
+}
+
+std::string FDyBasicGaugeBar::ToString()
+{
+  MDY_NOT_IMPLEMENTED_ASSERT();
+  return "";
+}
+
+void FDyBasicGaugeBar::pUpdatePercentage()
+{
+  this->mValues.mPercentage =
+      (this->mValues.mPresent - this->mValues.mMin)
+    / (this->mValues.mMax - this->mValues.mMin);
+}
+
+} /// ::dy namespace
