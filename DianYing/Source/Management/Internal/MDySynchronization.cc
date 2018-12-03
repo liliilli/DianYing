54775cbca1e20be1b8120c033f150cdf4e718ca8
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2018-12-03 20:28:46 +0900

    [Add] Add Synchronization manager for synchronizing other threads.

---
+++ DianYing/Source/Management/Internal/MDySynchronization.cc
@@ -0,0 +1,31 @@
+#include <precompiled.h>
+///
+/// MIT License
+/// Copyright (c) 2018 Jongmin Yun
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+///
+
+/// Header file
+#include <Dy/Management/Internal/MDySynchronization.h>
+
+namespace dy
+{
+
+EDySuccess MDySynchronization::pfInitialize()
+{
+  return DY_SUCCESS;
+}
+
+EDySuccess MDySynchronization::pfRelease()
+{
+  return DY_SUCCESS;
+}
+
+} /// ::dy namespace
