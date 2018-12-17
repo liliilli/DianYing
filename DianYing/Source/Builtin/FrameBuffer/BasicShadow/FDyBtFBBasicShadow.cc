e4a8b7d348c922ddf9912d4757e9e93621771e52
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2018-12-18 01:06:59 +0900

    [Add] Framebuffer & Attachments for replace old logics.

---
+++ DianYing/Source/Builtin/FrameBuffer/BasicShadow/FDyBtFBBasicShadow.cc
@@ -0,0 +1,32 @@
+#include <precompiled.h>
+///
+/// MIT License
+/// Copyright (c) 2018 Jongmin Yun
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+///
+
+/// Header file
+#include <Dy/Builtin/FrameBuffer/BasicShadow/FDyBtFBBasicShadow.h>
+#include <Dy/Builtin/Attachment/BasicShadow/BasicShadow.h>
+
+namespace dy
+{
+
+void FDyBtFBBasicShadow::ConstructBuffer(_MOUT_ PDyGlFrameBufferInstanceMetaInfo& property) noexcept
+{
+  property.mSpecifierName   = sName;
+  property.mFrameBufferSize = DDyVectorInt2{1280, 720};
+  property.mIsUsingDefaultDepthBuffer = false;
+  property.mIsNotUsingPixelShader     = true;
+
+  property.mAttachmentList.emplace_back(MSVSTR(FDyBtAttachmentBasicShadow::sName), EDyGlAttachmentType::Depth);
+}
+
+} /// ::dy namespace
