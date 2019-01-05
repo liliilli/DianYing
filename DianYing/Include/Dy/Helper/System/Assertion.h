68cc69cba640744aeee83062f7dc7f276417bd60
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2019-01-05 17:08:19 +0900

    [Fix] Fixed memory leak of assertion.

--- DianYing/Include/Dy/Helper/System/Assertion.h
+++ DianYing/Include/Dy/Helper/System/Assertion.h
@@ -15,6 +15,18 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <spdlog/fmt/fmt.h>
+
+#if defined(_WIN32) == true
+#include <Windows.h>
+#endif
+
+#if defined(max)
+#undef max
+#endif
+#if defined(min)
+#undef min
+#endif
 
 namespace dy
 {
@@ -73,12 +85,10 @@ namespace dy
 /// Implementation
 /// ---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*---*
 
-///
 /// @brief
 /// @param function
 /// @param update
-///
-FORCEINLINE void __ReleaseFunction(void (*function)(void), bool update) {
+inline void __ReleaseFunction(void (*function)(void), bool update) {
   static void (*s_function)(void) = nullptr;
 
   if (update) { s_function = function; }
@@ -94,22 +104,22 @@ FORCEINLINE void __ReleaseFunction(void (*function)(void), bool update) {
 ///
 /// @todo Implement third logger library message printing.
 ///
-FORCEINLINE void __EnhancedAssert( const char* expression,  bool result,  const char* filePath,  int line,  const char* failedMessage)
+inline void __EnhancedAssert( const char* expression,  bool result,  const char* filePath,  int line,  const char* failedMessage)
 {
   if (result == false)
   {
-    const auto msg = fmt::format(
-R"dy(Assert Failed : {0},
-Expected : {1},
-Sourtce : {2},
-Line : {3})dy",
-        failedMessage, expression, filePath, line);
-    std::printf("%s\n", msg.c_str());
+    {
+      const auto msg = fmt::format(R"dy(Assert Failed : {0}, Expected : {1}, Sourtce : {2}, Line : {3})dy",
+          failedMessage, expression, filePath, line);
+      std::printf("%s\n", msg.c_str());
 
-#if defined(_WIN32)
-    std::wstring winMsg{msg.begin(), msg.end()};
-    MessageBox(nullptr, winMsg.c_str(), L"Error!", MB_ICONWARNING | MB_OK);
-#endif
+      #if defined(_WIN32) == true
+      {
+        std::wstring winMsg{msg.begin(), msg.end()};
+        MessageBox(nullptr, winMsg.c_str(), L"Error!", MB_ICONWARNING | MB_OK);
+      }
+      #endif
+    }
 
     __ReleaseFunction(nullptr, false);
     exit(1);
@@ -124,19 +134,19 @@ Line : {3})dy",
 ///
 /// @todo Implement third logger library message printing.
 ///
-FORCEINLINE void __NotImplementedAssert( const char* filePath,  int line)
+inline void __NotImplementedAssert( const char* filePath,  int line)
 {
-  const auto msg = fmt::format(
-R"dy(This function is not implemented yet.
-Source : {0},
-Line : {1}
-)dy", filePath, line);
-  std::printf("%s\n", msg.c_str());
+  {
+    const auto msg = fmt::format(R"dy(This function is not implemented yet. Source : {0}, Line : {1})dy", filePath, line);
+    std::printf("%s\n", msg.c_str());
 
-#if defined(_WIN32)
-  std::wstring winMsg{msg.begin(), msg.end()};
-  MessageBox(nullptr, winMsg.c_str(), L"Error!", MB_ICONWARNING | MB_OK);
-#endif
+    #if defined(_WIN32) == true
+    {
+      std::wstring winMsg{msg.begin(), msg.end()};
+      MessageBox(nullptr, winMsg.c_str(), L"Error!", MB_ICONWARNING | MB_OK);
+    }
+    #endif
+  }
 
   __ReleaseFunction(nullptr, false);
   exit(1);
@@ -146,21 +156,22 @@ Line : {1}
 /// @brief
 /// Unexpected branch entering.
 ///
-FORCEINLINE void __UnexpectedBranch( const char* filePath,  int line)
+inline void __UnexpectedBranch( const char* filePath,  int line)
 {
-  const auto msg = fmt::format(
-R"dy(Unexpected branch not intended.
-Source : {0},
-Line : {1}
-)dy", filePath, line);
-  std::printf("%s\n", msg.c_str());
+  {
+    const auto msg = fmt::format(R"dy(Unexpected branch not intended. Source : {0}, Line : {1})dy", filePath, line);
+    std::printf("%s\n", msg.c_str());
 
-#if defined(_WIN32)
-  std::wstring winMsg{msg.begin(), msg.end()};
-  MessageBox(nullptr, winMsg.c_str(), L"Error!", MB_ICONWARNING | MB_OK);
-#endif
+    #if defined(_WIN32) == true
+    {
+      std::wstring winMsg{msg.begin(), msg.end()};
+      MessageBox(nullptr, winMsg.c_str(), L"Error!", MB_ICONWARNING | MB_OK);
+    }
+    #endif
 
-  printf("Unexpected branch not intended.\n" "Source : \t%s, line : %d\n", filePath, line);
+    printf("Unexpected branch not intended.\n" "Source : \t%s, line : %d\n", filePath, line);
+  }
+
   __ReleaseFunction(nullptr, false);
   exit(1);
 }
