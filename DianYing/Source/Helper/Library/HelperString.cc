526ef537edcb1f971c3348a740141fb1b7c396df
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2019-01-09 02:08:08 +0900

    [Fix] Move implementation of HelperString to .cc file to avoid lnk err

--- DianYing/Source/Helper/Library/HelperString.cc
+++ DianYing/Source/Helper/Library/HelperString.cc
@@ -1,5 +1,4 @@
-#ifndef GUARD_DY_HELPER_HELPERSTRING_H
-#define GUARD_DY_HELPER_HELPERSTRING_H
+#include <precompiled.h>
 ///
 /// MIT License
 /// Copyright (c) 2018-2019 Jongmin Yun
@@ -13,19 +12,26 @@
 /// SOFTWARE.
 ///
 
-#include <algorithm>
-#include <string>
-#include <random>
-
-#include <Dy/Helper/System/Macroes.h>
-#include <Dy/Helper/GlobalType.h>
+/// Header file
+#include <Dy/Helper/HelperString.h>
 
 namespace dy
 {
 
 /// @brief Get random string wich length count.
-MDY_NODISCARD std::string DyGetRandomString(_MIN_ TU32 length);
+MDY_NODISCARD std::string DyGetRandomString(_MIN_ TU32 length)
+{
+  static std::default_random_engine Rng(std::random_device{}());
+  auto randchar = []() -> char
+  {
+    constexpr char charset[]  = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    constexpr auto maxIndex   = (sizeof(charset) - 1);
+    return charset[Rng() % maxIndex];
+  };
 
-} /// ::dy namesapce
+  std::string str(length, 0);
+  std::generate_n(str.begin(), length, randchar);
+  return str;
+}
 
-#endif /// GUARD_DY_HELPER_HELPERSTRING_H
+} /// ::dy namespace
