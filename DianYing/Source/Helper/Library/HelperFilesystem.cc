9a65b70acfaaad81cd7bf57a53aebdf522e55624
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2019-01-21 15:55:05 +0900

    [Add] Add Helper filesystem features.

---
+++ DianYing/Source/Helper/Library/HelperFilesystem.cc
@@ -0,0 +1,28 @@
+#include <precompiled.h>
+///
+/// MIT License
+/// Copyright (c) 2018-2019 Jongmin Yun
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+///
+
+/// Header file
+#include <Dy/Helper/Library/HelperFilesystem.h>
+#include <filesystem>
+
+namespace dy
+{
+
+bool DyFsIsFileExist(_MIN_ const std::string& iFilePath)
+{
+  namespace fs = std::filesystem;
+  return fs::exists(iFilePath);
+}
+
+} /// ::dy namespace
