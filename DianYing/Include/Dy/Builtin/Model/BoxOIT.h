6a8a1fe25a9c36380a5bef53d6f7f83964ec2f12
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2019-02-23 16:40:49 +0900

    [Add] Add test box OIT model

---
+++ DianYing/Include/Dy/Builtin/Model/BoxOIT.h
@@ -0,0 +1,32 @@
+#ifndef GUARD_DY_BUILTIN_MODEL_BOX_H
+#define GUARD_DY_BUILTIN_MODEL_BOX_H
+///
+/// MIT License
+/// Copyright (c) 2018-2019 Jongmin Yun
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+///
+
+#include <Dy/Builtin/Abstract/ADyModelResource.h>
+
+namespace dy::builtin
+{
+
+///
+/// @class FDyBuiltinModelBoxOIT
+/// @brief Create OIT model box information and resources.
+///
+class FDyBuiltinModelBoxOIT final : public ADyModelResource
+{
+  MDY_REGISTER_RESOURCE_MODEL(FDyBuiltinModelBoxOIT, "dyBtModelBoxOIT");
+};
+
+} /// ::dy::builtin namespace
+
+#endif /// GUARD_DY_BUILTIN_MODEL_BOX_H
