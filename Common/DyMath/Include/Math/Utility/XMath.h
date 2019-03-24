9f1bc6a47f7ef9c3d1b10220f3af606f04979e64
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2019-03-25 02:34:43 +0900

    Add clamp value api to common math function

--- Common/DyMath/Include/Math/Utility/XMath.h
+++ Common/DyMath/Include/Math/Utility/XMath.h
@@ -32,22 +32,34 @@ constexpr TType k2Pi = TType(3.1415926535897932385L) * TType(2);
 template <typename TType>
 constexpr TType kExp = TType(2.71828182845904523536L);
 
+/// @brief Convert degree value to radian value.
+template <typename TType>
+constexpr TType kToRadian = kPi<TType> / TType(180);
+
+/// @brief Convert radian value to degree value.
+template <typename TType>
+constexpr TType kToDegree = TType(180) / kPi<TType>;
+
+/// @brief Map given value to [start, end).
+template <typename TType>
+constexpr TType MapValue(TType value, TType start, TType end) noexcept;
+
 /// @brief Check float lhs is nearly equal to rhs.
 /// floating-point number type like a float and double has a natural problem like
 /// a floating-error such as 0.1 but 0.10000007, so you have to use this function
 /// if you want to compare two floating points.
-constexpr bool IsNearlyEqual(TF32 lhs, TF32 rhs, TF32 error_tolerance = 0.001f) noexcept
+constexpr bool IsNearlyEqual(TF32 lhs, TF32 rhs, TF32 tolerance = 0.001f) noexcept
 {
-  return (rhs < lhs ? lhs - rhs : rhs - lhs) < error_tolerance;
+  return (rhs < lhs ? lhs - rhs : rhs - lhs) < tolerance;
 }
 
 /// @brief Check double lhs is nearly equal to rhs.
 /// floating-point number type like a float and double has a natural problem like
 /// a floating-error such as 0.1 but 0.10000007, so you have to use this function
 /// if you want to compare two double points.
-constexpr bool IsNearlyEqual(TF64 lhs, TF64 rhs, TF64 error_tolerance = 0.0001) noexcept
+constexpr bool IsNearlyEqual(TF64 lhs, TF64 rhs, TF64 tolerance = 0.001) noexcept
 {
-  return (rhs < lhs ? lhs - rhs : rhs - lhs) < error_tolerance;
+  return (rhs < lhs ? lhs - rhs : rhs - lhs) < tolerance;
 }
 
 /// @brief Check given two vector values are equal within given tolerance value. 
@@ -75,4 +87,5 @@ template <typename TLeft, typename TRight>
 bool IsNearlyEqual(const DVector4<TLeft>& lhs, const DVector4<TRight>& rhs, TReal tolerance);
 
 } /// ::dy::math namespace
+#include <Math/Utility/Inline/XMath/MapValue.inl>
 #include <Math/Utility/Inline/XMath/IsNearlyEqual.inl>
