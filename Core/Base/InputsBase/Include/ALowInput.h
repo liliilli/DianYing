293667978413e50ece115f0d235a79b214e7e66b
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2019-04-15 22:31:38 +0900

    Add ALowInput for low-level input binding module

---
+++ Core/Base/InputsBase/Include/ALowInput.h
@@ -0,0 +1,43 @@
+#pragma once
+///
+/// MIT License
+/// Copyright (c) 2018-2019 Jongmin Yun
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/// SOFTWARE.
+///
+
+#include <DInputButton.h>
+#include <ELowKeyboard.h>
+#include <ELowMouse.h>
+
+namespace dy::base 
+{
+
+/// @class ALowInput
+/// @brief Low-input management type.
+class ALowInput
+{
+public:
+  ALowInput() = default;
+  virtual ~ALowInput() = 0;
+
+  /// @brief Update keyboard key with derived platform descriptor instance.
+  virtual void UpdateKeyboard(void* descriptor) = 0;
+
+  /// @brief Update mouse button with derived platform descriptor instance.
+  virtual void UpdateMouseButton(void* descriptor) = 0;
+
+protected:
+  std::array<DInputButtonItem, Dy_Key_Menu>   sLowKeyboards;
+  std::array<DInputButtonItem, DyMouse__Sum>  sLowMouseButtons;
+};
+
+inline ALowInput::~ALowInput() = default;
+
+} /// ::dy::base namespace
