a962101ea0255a610e41aa7152c1d1e4cbde0443
Jongmin Yun (Neu.) <jmyundev@gmail.com>
2019-04-24 13:42:12 +0900

    Update

--- Test/TestInput/Source/XMain.cc
+++ Test/TestInput/Source/XMain.cc
@@ -18,12 +18,10 @@
 #include <PLowInputMousePos.h>
 #include <cassert>
 
-static wchar_t szWindowClass[] = L"win32app";
 const wchar_t* windowName = L"Gainput basic sample";
 bool doExit = false;
 
 std::unique_ptr<dy::APlatformBase> platform = nullptr;
-HWND gHwnd; // Window handle. 
 
 int WINAPI WinMain(
   [[maybe_unused]] HINSTANCE hInstance, 
@@ -52,44 +50,6 @@ int WINAPI WinMain(
 
   auto optRes = platform->CreateWindow(desc);
   assert(optRes.has_value() == true);
-
-#if 0
-	WNDCLASSEX wcex;
-	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.style          = CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc    = WndProc;
-	wcex.cbClsExtra     = 0;
-	wcex.cbWndExtra     = 0;
-	wcex.hInstance      = hInstance;
-	wcex.hIcon          = 0;
-	wcex.hCursor        = LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-	wcex.lpszMenuName   = NULL;
-	wcex.lpszClassName  = szWindowClass;
-	wcex.hIconSm        = 0;
-
-	if (!RegisterClassEx(&wcex))
-	{
-		MessageBox(NULL, L"Call to RegisterClassEx failed!", L"Gainput basic sample", NULL);
-		return 1;
-	}
-
-	gHwnd = CreateWindow(
-    szWindowClass,
-    windowName,
-    WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
-    800, 600,
-    NULL, NULL, hInstance, NULL);
-
-	if (!gHwnd)
-	{
-		MessageBox(NULL, L"Call to CreateWindow failed!", L"Gainput basic sample", NULL);
-		return 1;
-	}
-
-	ShowWindow(gHwnd, nCmdShow);
-	UpdateWindow(gHwnd);
-#endif
 
   auto& input = platform->GetInputManager();
   input.SetMousePosFeatureState(dy::base::ELowMousePosState::Normal);
